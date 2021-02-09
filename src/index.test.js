2ba0e19789a875fcb9e40d92adbe13f5634bf8e6
Lari Hotari <lari@hotari.net>
2021-02-09 12:31:01 +0200

    Install upterm on Linux by downloading the binary release

--- src/index.test.js
+++ src/index.test.js
@@ -34,8 +34,23 @@ describe('upterm GitHub integration', () => {
     const customConnectionString = "foobar"
     execShellCommand.mockReturnValue(Promise.resolve(customConnectionString))
     await run()
-    expect(execShellCommand).toHaveBeenNthCalledWith(1, "brew install owenthereal/upterm/upterm")
+    expect(execShellCommand).toHaveBeenNthCalledWith(1, "curl -L https://github.com/owenthereal/upterm/releases/download/0.5.2/linux-amd64-v0.5.2.tar.gz | tar zxvf - --strip-components=1 --wildcards '*/upterm' && sudo mv upterm /usr/local/bin/")
+    expect(execShellCommand).toHaveBeenNthCalledWith(2, "sudo apt-get -y install tmux")
     expect(core.info).toHaveBeenNthCalledWith(1, `${customConnectionString}`);
     expect(core.info).toHaveBeenNthCalledWith(2, "Exiting debugging session because '/continue' file was created");
   });
+  it('should install using brew on macos', async () => {
+    Object.defineProperty(process, "platform", {
+      value: "darwin"
+    })
+    core.getInput.mockReturnValue("true")
+    const customConnectionString = "foobar"
+    execShellCommand.mockReturnValue(Promise.resolve(customConnectionString))
+    await run()
+    expect(execShellCommand).toHaveBeenNthCalledWith(1, "brew install owenthereal/upterm/upterm")
+    expect(execShellCommand).toHaveBeenNthCalledWith(2, "brew install tmux")
+    expect(core.info).toHaveBeenNthCalledWith(1, `${customConnectionString}`);
+    expect(core.info).toHaveBeenNthCalledWith(2, "Exiting debugging session because '/continue' file was created");
+  });
+
 });
