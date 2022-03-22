0a4cf57e995047afc0ad514400ae76fcf63a43fd
Lari Hotari <lari@hotari.net>
2022-03-22 10:22:30 +0200

    Install tmux with apt-get if it's not available

--- src/index.test.js
+++ src/index.test.js
@@ -38,7 +38,7 @@ describe('upterm GitHub integration', () => {
     execShellCommand.mockReturnValue(Promise.resolve(customConnectionString))
     await run()
     expect(execShellCommand).toHaveBeenNthCalledWith(1, "curl -sL https://github.com/owenthereal/upterm/releases/download/v0.7.6/upterm_linux_amd64.tar.gz | tar zxvf - -C /tmp upterm && sudo install /tmp/upterm /usr/local/bin/")
-    expect(execShellCommand).toHaveBeenNthCalledWith(2, "sudo apt-get -y install tmux")
+    expect(execShellCommand).toHaveBeenNthCalledWith(2, "if ! command -v tmux &>/dev/null; then sudo apt-get -y install tmux; fi")
     expect(core.info).toHaveBeenNthCalledWith(1, "Auto-generating ~/.ssh/known_hosts by attempting connection to uptermd.upterm.dev")
     expect(core.info).toHaveBeenNthCalledWith(2, "Creating a new session. Connecting to upterm server ssh://myserver:22")
     expect(core.info).toHaveBeenNthCalledWith(3, `${customConnectionString}`);
@@ -56,7 +56,7 @@ describe('upterm GitHub integration', () => {
     execShellCommand.mockReturnValue(Promise.resolve(customConnectionString))
     await run()
     expect(execShellCommand).toHaveBeenNthCalledWith(1, "curl -sL https://github.com/owenthereal/upterm/releases/download/v0.7.6/upterm_linux_amd64.tar.gz | tar zxvf - -C /tmp upterm && sudo install /tmp/upterm /usr/local/bin/")
-    expect(execShellCommand).toHaveBeenNthCalledWith(2, "sudo apt-get -y install tmux")
+    expect(execShellCommand).toHaveBeenNthCalledWith(2, "if ! command -v tmux &>/dev/null; then sudo apt-get -y install tmux; fi")
     expect(core.info).toHaveBeenNthCalledWith(1, "Appending ssh-known-hosts to ~/.ssh/known_hosts. Contents of ~/.ssh/known_hosts:")
     expect(core.info).toHaveBeenNthCalledWith(2, `${customConnectionString}`)
     expect(core.info).toHaveBeenNthCalledWith(3, "Creating a new session. Connecting to upterm server ssh://myserver:22")
